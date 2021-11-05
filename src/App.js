0deb6e9fd4ff428c560c5f38fc6b65756ed2eecb
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-05 12:40:50 -0700

    passed index[0] tp dom and added css to div

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character.js";
 
 const App = () => {
+  const [charList, setCharList] = useState([]);
+
+  useEffect(() => {
+    charInfo();
+  }, []);
+
+  const charInfo = () => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then((res) => {
+        console.log(res.data)
+        setCharList(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character charList={charList[0]} />
+
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import App from '../App';
+import style from '../App.css'
+
+
+
 // Write your Character component here
+const Character = (props) => {
+    console.log(props);
+
+    return (
+        <div className= "charName">
+            <h3>Luke Skywalker</h3>
+                <ul>
+                    <li>
+                        gender:
+                    </li>
+                    <li>
+                        age:
+                    </li>
+                </ul>
+        </div>
+    )
+}
+
+
+
+
+
+export default Character
